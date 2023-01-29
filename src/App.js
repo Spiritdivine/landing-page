3e037b0063b0778d31f64b2f679087996278a082
Spirit Divine <spiritdivine777@gmail.com>
2023-01-29 18:54:12 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Page from './component/landing-page';
 import Second from './component/whiteblu/second';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Second/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' index element={<Page/>}/>
+          <Route path='second' element={<Second/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/Body/Navbar.js
+++ src/component/Body/Navbar.js
@@ -1,23 +1,25 @@
 import Logo from "./logo.png";
 import { FiShoppingCart } from "react-icons/fi";
 import Sidebar from "../Sidebar";
+import { Outlet, Link } from "react-router-dom";
 
 const Navbar = () => {
 
     return ( 
         <nav>
             <div className="navi">
-                <div className="logo"><img src={Logo} alt="logo" ></img></div>
+                <div className="logo"><Link to="/"><img src={Logo} alt="logo" ></img></Link></div>
                 <div className="myLinks">
                     <ul className="shoeNavs">
-                        <li>9 Elite</li>
+                        <li><Link to="second">9 Elite</Link></li>
+                        
                         <li>15 Elite</li>
                         <li>VIII Elite</li>
                         <li>Legend 9</li>
                         <li>Contact</li>
                         
                         <li className="cart"><FiShoppingCart/></li>
-                       
+                        
                     </ul>
                 </div>
                 <div className="burg">
@@ -26,6 +28,7 @@ const Navbar = () => {
                 
                 
             </div>
+            <Outlet/>
         </nav>
      );
 }
